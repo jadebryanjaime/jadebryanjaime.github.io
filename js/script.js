5a96b1d1f9d36dd2ffaef335ed8fb0e8458ac2df
JB <41357505+jadebryanjaime@users.noreply.github.com>
2022-02-23 15:21:44 +0800

    update portfolio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
-    // toggle menu/navbar
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active"); 
-        $('.menu-btn i').toggleClass("active"); 
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+  });
+  // toggle menu/navbar
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
 });
 
 window.onscroll = function () { scrollRotate() };
 function scrollRotate() {
-    var navimg = document.getElementById("logo-center");
-    navimg.style.transform = "rotate(" + (window.pageYOffset / 1) + "deg)"
+  var navimg = document.getElementById("logo-center");
+  navimg.style.transform = "rotate(" + (window.pageYOffset / 1) + "deg)"
 }
 
 const checkpoint = 300;
- 
+
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
   if (currentScroll <= checkpoint) {
@@ -29,4 +29,25 @@ window.addEventListener("scroll", () => {
     opacity = 0;
   }
   document.querySelector("#logo-center").style.opacity = opacity;
+});
+
+var xValues = ["Angular", "C# .Net Core", "MS SQL Server", "Wordpress", "HTML5", "CSS", "TypeScript"];
+var yValues = [10, 10, 8, 9, 8, 8, 10];
+var barColors = ["#d63031", "#0984e3", "#2d3436", "#b2bec3", "#00b894", "#fdcb6e"];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Skills"
+    }
+  }
 });
